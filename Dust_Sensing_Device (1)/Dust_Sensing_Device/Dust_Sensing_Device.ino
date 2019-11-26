52bb93abe8c3e98d996c540beeba4fb75a0fee6b
donghyeoncheon <58211121+donghyeoncheon@users.noreply.github.com>
2019-11-26 18:20:16 +0900

    Add files via upload

---
+++ Dust_Sensing_Device (1)/Dust_Sensing_Device/Dust_Sensing_Device.ino
@@ -0,0 +1,139 @@
+#include "LiquidCrystal_I2C.h"
+#include "DHT.h"
+#include "emotion.h"
+#include "PMsensor.h"
+
+#define sensitivity  0.1
+//미세먼지센서의 센싱 민감도 설정
+//민감도의 숫자가 클 경우 : 센서 값의 변화가 민감함
+//민감도의 숫자가 작을 경우 : 센서 값의 변화가 둔함
+
+#define DHTPIN 6
+#define DHTTYPE DHT11
+
+const int sensorPin = A0;
+const int sensorLED = 3;
+
+PMsensor PM;
+LiquidCrystal_I2C lcd(LCD 이름, 16, 2);
+//LCD 이름 : 0x27 또는 0x3F 입력
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setEmoticon(float data) {
+  if (data > 100) {
+    lcd.createChar(4, topAngry1);
+    lcd.createChar(5, topAngry2);
+    lcd.createChar(6, bottomAngry1);
+    lcd.createChar(7, bottomAngry2);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+  }
+  else if (data > 50) {
+    lcd.createChar(4, topSoSo1);
+    lcd.createChar(5, topSoSo2);
+    lcd.createChar(6, bottomSoSo1);
+    lcd.createChar(7, bottomSoSo2);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(11, LOW);
+  }
+
+  else {
+    lcd.createChar(4, topSmile1);
+    lcd.createChar(5, topSmile2);
+    lcd.createChar(6, bottomSmile1);
+    lcd.createChar(7, bottomSmile2);
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(11, LOW);
+  }
+}
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+  lcd.createChar((byte)0, temp);
+  lcd.createChar(1, humi);
+  lcd.createChar(2, dust);
+  lcd.createChar(3, C);
+
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  Serial.begin(9600);
+
+  dht.begin();
+  PM.init(sensorLED, sensorPin);
+}
+
+void loop() {
+  float humi = dht.readHumidity();
+  float temp = dht.readTemperature();
+
+  if (isnan(humi) || isnan(temp) ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  float data = 0;
+  int err = PMsensorErrSuccess;
+
+  if ((err = PM.read(&data, true, sensitivity)) != PMsensorErrSuccess) {
+    Serial.print("data Error = ");
+    Serial.println(err);
+    delay(1500);
+    return;
+  }
+  Serial.println(data);
+  delay(1500);
+  setEmoticon(data);
+  lcd.setCursor(0, 0);
+  lcd.write(2);
+  lcd.print(" ");
+  lcd.print(data);
+
+  if (data > 100)
+    lcd.print("ug");
+  else
+    lcd.print(" ug");
+  if ((err = PM.read(&data, true, sensitivity)) != PMsensorErrSuccess) {
+    Serial.print("data Error = ");
+    Serial.println(err);
+    delay(1500);
+    return;
+  }
+  Serial.println(data);
+  delay(1500);
+  setEmoticon(data);
+  lcd.setCursor(0, 0);
+  lcd.write(2);
+  lcd.print(" ");
+  lcd.print(data);
+
+  if (data > 100)
+    lcd.print("ug");
+  else
+    lcd.print(" ug");
+
+  lcd.setCursor(0, 1);
+  lcd.write((byte)0);
+  lcd.print(" ");
+  lcd.print((int)temp);
+  lcd.write(3);
+
+  lcd.print(" ");
+  lcd.write(1);
+  lcd.print(" ");
+  lcd.print((int)humi);
+  lcd.print("%");
+
+  lcd.setCursor(13, 0);
+  lcd.write(4);
+  lcd.write(5);
+
+  lcd.setCursor(13, 1);
+  lcd.write(6);
+  lcd.write(7);
+}
